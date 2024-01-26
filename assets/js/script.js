039599da6e8a1574370dc4a1c998362b4c171f2b
Lorenzo Figueroa Wlack <lorenzo.f.w12@gmail.com>
2024-01-26 12:17:22 -0300

    primer push

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* recargar al tocar la imagen del logo */
+$(document).ready(function () {
+  $("#viajesChileLink").click(function (e) {
+    e.preventDefault();
+    location.reload();
+  });
+});
