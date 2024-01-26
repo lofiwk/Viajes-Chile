89edb2a64f99a6e663313b4019924cc613daddfa
Lorenzo Figueroa Wlack <lorenzo.f.w12@gmail.com>
2024-01-26 13:28:20 -0300

    implementacion de scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,21 @@ $(document).ready(function () {
     location.reload();
   });
 });
+
+/* tooltip */
+document.addEventListener("DOMContentLoaded", function () {
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+  // Agregar evento clic al botón "Enviar"
+  document
+    .getElementById("enviarButton")
+    .addEventListener("click", function () {
+      // Mostrar mensaje de alerta
+      alert("El mensaje fue enviado con éxito");
+    });
+});
